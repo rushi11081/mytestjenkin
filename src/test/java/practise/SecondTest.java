42f340694a250e255d51caa6fc0c7cf797dcb531
rushikesh <rushi11081@gmail.com>
2022-09-21 18:19:34 +0530

    new class added

---
+++ src/test/java/practise/SecondTest.java
@@ -0,0 +1,13 @@
+package practise;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	
+	@Test
+	  public void get2()
+	  {
+		  System.out.println("This is second test");
+	  }
+}
