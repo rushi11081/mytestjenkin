7678cd10804ccae9f90006a580ba4ced32e8cc0e
rushikesh <rushi11081@gmail.com>
2022-09-21 18:02:41 +0530

    data add

---
+++ src/test/java/practise/FirstTest.java
@@ -0,0 +1,14 @@
+package practise;
+
+import org.testng.annotations.Test;
+
+public class FirstTest 
+{
+
+	  @Test
+	  public void get()
+	  {
+		  System.out.println("This is first test");
+	  }
+	
+}
